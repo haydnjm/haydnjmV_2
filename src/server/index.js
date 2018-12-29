46f6911977c23f979be4312efb19a41559790ca2
unknown <haydnjmorris@googlemail.com>
2018-12-29 21:31:47 +0000

    port change

--- src/server/index.js
+++ src/server/index.js
@@ -67,6 +67,10 @@ server.get('*', (req, res, next) => {
   })
 })
 
-server.listen(3000, () => console.log('listening on port 3000'))
+const PORT = process.env.PORT || 4000
+
+console.log('port = ', PORT)
+
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`))
 
 server.use(express.static('public'))
